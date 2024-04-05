786a2a48ddae0c7b870d38744c30c8ad8edc8ce2
Cshedez1 <161688060+Cshedez1@users.noreply.github.com>
2024-04-05 08:15:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,12 +79,11 @@ st.success("You did it !")
 st.error("Error")
 st.warning("Warning")
 st.info("It's easy to build a streamlit app")
-st.exception(RuntimeError("RuntimeError exception"))
 
 
 #MAKING GRAPHS
 st.caption("Lesson 5 - Displaying Graphs") #Caption
-
+pip install matplotlib
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
